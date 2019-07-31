dc92cfc9d728a17ddf9eb59f6c8d903c33bb0436
Eric Hendrix <agentmrbig@gmail.com>
2019-07-30 20:22:27 -0400

    Basic middleware skeleton setup

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const express = 'express';
+const express = require('express');
+const cors = require('cors');
+const posts = require('./data/posts/postDb');
+const users = require('./data/users/userDb');
+
+const port = 5000;
 
 const server = express();
 
@@ -8,8 +13,57 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {
-
+const logger = (reqMethod, reqUrl, timestamp) => {
+  timestamp = Date.now();
+  console.log(`Request method: ${reqMethod}`, `Request URL: ${reqUrl}`, `Timestamp: ${timestamp}`)
 };
 
-module.exports = server;
+const validateUserId = (req, res, next, id) => {
+  //const { user_id } = req.body;
+  if (!id) {
+    // need to check if id match any user id in database
+    res
+      .status(404)
+      .json({ success: false, errorMessage: 'invalid user id' })
+  } else {
+    // store user object as req.user
+  }
+};
+
+const validateUser = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!req.body) {
+    res
+      .status(400)
+      .json({ success: false, errorMessage: 'missing user data' })
+  } else if (!name) {
+    res
+      .status(400)
+      .json({ success: false, errorMessage: 'missing required name field' })
+  } else {
+    res
+      .status(200)
+      .json({ success: true, message: 'User Validated' })
+  }
+};
+
+const validatePost = (req, res, next) => {
+  const { text } = req.body;
+
+  if (!req.body) {
+    res
+      .status(400)
+      .json({ success: false, errorMessage: 'missing post data' })
+  } else if (!text) {
+    res
+      .status(400)
+      .json({ success: false, errorMessage: 'missing required name field' })
+  } else {
+    res
+      .status(200)
+      .json({ success: true, message: 'Post validated' })
+  }
+}
+
+server.listen(port, () => console.log(`Server listening on ${port}`));
